c94121850548be20b4543e0ea1a0b749e7566fa3
donec <donec20152015@yandex.ru>
2018-08-20 23:40:46 +0300

    Наконец - то сделал ДЗ 1

--- HomeWork_001/index.php
+++ HomeWork_001/index.php
@@ -3,15 +3,15 @@
 $b = "<br>";
 
 //task 1
-echo "<h3>Задача 1</h3>".$b;
+echo "<h3>Задание № 1</h3>".$b;
 $name = "Андрей";
 $age = 24;
 echo "Меня зовут: ".$name.$b;
-echo "Мне ".$age." лет".$b;
+echo "Мне ".$age." года".$b;
 echo "\" ! | \\ / ' \"  \\".$b;
 
 //task 2
-echo "<h3>Задача 2</h3>".$b;
+echo "<h3>Задание № 2</h3>".$b;
 define("FIGURES", 80);
 define("FELT_PEN", 40);
 define("PENCIL", 23);
@@ -21,15 +21,81 @@ $paints = $x;
 echo "Красками выполенно ".$paints." рисунков";
 
 //task 3
-echo "<h3>Задача 3</h3>".$b;
+echo "<h3>Задание № 3</h3>".$b;
+//Выставил значения $age от 1 до 100 так как использование случайного генерирования по mt_getrandmax() не целесообразно
 $age = mt_rand(1, 100);
-if (($age >= 18) && ($age <= 65)) {
-    echo "Вам еще работатьи работать";
-    if ($age > 65) {
-        echo "Вам пора на пенсию";
-    }
-} elseif (($age >= 1) && ($age <= 17)) {
+if ($age >= 18 && $age <= 65) {
+    echo "Вам еще работать и работать";
+} elseif ($age > 65) {
+    echo "Вам пора на пенсию";
+} elseif ($age >= 1 && $age <= 17) {
     echo "Вам ещё рано работать";
 } else {
     echo "Неизвестный возраст";
 }
+
+//task 4
+echo "<h3>Задание № 4</h3>".$b;
+$day = mt_rand(1, 10);
+switch (true) {
+    case ($day >=1 && $day <= 5):
+        echo "Это рабочий день";
+        break;
+    case ($day >=6 && $day <= 7):
+        echo "Это выходной день";
+        break;
+    default:
+        echo "Неизвестный день";
+}
+
+//task 5
+echo "<h3>Задание № 5</h3>".$b;
+
+$bmw = array(
+    "model" => "X5",
+    "speed" => 120,
+    "doors" => 5,
+    "year" => 2015,
+);
+$toyota = array(
+    "model" => "Camry",
+    "speed" => 239,
+    "doors" => 4,
+    "year" => 2018,
+);
+$opel = array(
+    "model" => "Astra",
+    "speed" => 190,
+    "doors" => 3,
+    "year" => 2014,
+);
+
+$cars = array_merge_recursive($bmw, $toyota, $opel);
+//C этим заданием не мало првозился думал как провести в цикл разные варианты опробывал с for и while и foreach
+echo "CAR bmw".$b;
+echo $cars["model"][0]." ".$cars["speed"][0]." ".$cars["doors"][0]." ".$cars["year"][0].$b;
+echo "CAR toyota".$b;
+echo $cars["model"][1]." ".$cars["speed"][1]." ".$cars["doors"][1]." ".$cars["year"][1].$b;
+echo "CAR opel".$b;
+echo $cars["model"][2]." ".$cars["speed"][2]." ".$cars["doors"][2]." ".$cars["year"][2].$b;
+
+//task 6
+echo "<h3>Задание № 6</h3>".$b;
+
+$table = '<table>';
+for ($i = 1; $i <= 10; $i++) {
+    $table .= '<tr>';
+    for ($c = 1; $c <= 10; $c++) {
+        if (($i % 2) == 0 && ($c % 2) == 0) {
+            $result = "(".$i*$c.")";
+        } elseif (($i % 2) != 0 && ($c % 2) != 0) {
+            $result = "[".$i*$c."]";
+        } else {
+            $result = $i*$c;
+        }
+        $table .= '<td>'.$result.'</td>';
+    }
+    $table .= '</tr>';
+}
+$table .= '<table>';
+echo $table;
