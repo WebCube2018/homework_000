61c8280e17608c178c301e79b4dcc30066bd0999
donec <donec20152015@yandex.ru>
2018-09-05 22:25:34 +0300

    Код готовый вариант осталось поработаь над PSR2 ругается на классы

--- HomeWork_004/index.php
+++ HomeWork_004/index.php
@@ -4,7 +4,7 @@ require_once "class.php";
 
 //Вывод инфы
 try {
-    $tar = new TariffBsic();
+    $tar = new TariffBsic();//Пишем тариф
     echo $tar->calculation(5, 1471, 23, get_class($tar));
 } catch (Exception $error) {
     print "Ошибка!!!: ".$error->getMessage();
